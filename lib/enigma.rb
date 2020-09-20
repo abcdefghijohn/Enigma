f26ff8297d7b861a7371786e1b3aec9bce378158
John Kim <abcdefghijohnk@gmail.com>
2020-09-20 13:01:48 -0600

    create get last four test, pass

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -5,8 +5,28 @@ class Enigma
     @alphabet = ("a".."z").to_a << " "
   end
 
-  def generate_date
-    Date.today.strftime("%d%m%y")
+  def generate_date_key
+    today = Date.today.strftime("%d%m%y")
+    today.to_i ** 2
   end
 
+  def last_four_of_date
+    generate_date_key.digits.reverse[-4..-1]
+  end
+
+  def generate_key
+    rand(9999).to_s.rjust(5, "0")
+  end
 end
+
+
+
+
+
+# 1. need date
+# 2. need key
+# 3. need to assign key according to ABCD
+# 4. need to square date
+# 5. need to grab last 4
+# 6. need to offset ABCD accordingly to last 4
+# 7. need to encryt using ABCD
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -21,17 +21,23 @@ class EnigmaTest < Minitest::Test
     assert_equal expected, enigma.encrypt("hello world", "02715", "040895")
   end
 
-  def test_it_can_get_date
+  def test_it_can_get_date_key
     enigma = Enigma.new
     Date.stubs(:today).returns(Date.new(2020, 9, 20))
 
-    assert_equal '200920', enigma.generate_date
+    assert_equal 40368846400, enigma.generate_date_key
   end
 
-  def test_it_can_get_keys
+  def test_it_can_get_key
     enigma = Enigma.new
     enigma.stubs(:rand).returns(8472)
 
     assert_equal '08472', enigma.generate_key
+  end
+
+  def test_it_can_get_last_four
+    enigma = Enigma.new
+    Date.stubs(:today).returns(Date.new(2020, 9, 20))
+    assert_equal [6,4,0,0], enigma.last_four_of_date
   end
 end
