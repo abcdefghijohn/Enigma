3a33d4095dbd68c7afe1f5603c5117db0cc5627d
John Kim <abcdefghijohnk@gmail.com>
2020-09-21 13:44:32 -0600

    create encrypt cli file

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,1 +1,15 @@
+require_relative 'enigma'
 
+handle = File.open(ARGV[0], 'r')
+
+incoming_text = handle.read
+handle.close
+
+enigma = Enigma.new
+encrypted = enigma.encrypt(incoming_text)
+
+writer = File.open(ARGV[1], 'w')
+writer.write(encrypted[:encryption])
+writer.close
+
+puts "Created #{ARGV[1]} with the key #{encrypted[:key]} and date #{encrypted[:date]}"
