70126126d3741fda3825cdc5e0144e20095d1c24
John Kim <abcdefghijohnk@gmail.com>
2020-09-22 01:46:15 -0600

    decrypt cli file made and works

---
+++ lib/decrypt.rb
@@ -0,0 +1,15 @@
+require_relative 'enigma'
+
+handle = File.open(ARGV[0], 'r')
+
+incoming_text = handle.read
+handle.close
+
+enigma = Enigma.new
+decrypted = enigma.decrypt(incoming_text, ARGV[2], ARGV[3])
+
+writer = File.open(ARGV[1], 'w')
+writer.write(decrypted[:decryption])
+writer.close
+
+puts "Created #{ARGV[1]} with the key #{decrypted[:key]} and date #{decrypted[:date]}"
